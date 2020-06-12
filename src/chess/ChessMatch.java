cb857bb8ab2f2239fbfcdedaf1886010301e8904
Pedro Augusto <pedroet19@gmail.com>
2020-06-12 17:06:24 -0300

    Classes de execeção e posição

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,10 +25,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) { 
+		board.placePiece(piece, new ChessPosition(column, row).toposition());
+	}
+	
 	public void initialSetup() {
-		board.placePiece(new Rook(board, Color.White) , new position(2, 1));
-		board.placePiece(new King(board, Color.Black), new position(0, 4));
-		board.placePiece(new King(board, Color.White), new position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8,  new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		
 	}
 }
